b43f54a8cfb0fd94841f301d6f245c182759a323
Jasleen <jaysleenk11@gmail.com>
2023-12-04 00:40:31 -0500

    My first commit

---
+++ Untitled-1.js
@@ -0,0 +1,24 @@
+// script.js
+function userChoice(choice) {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * 3);
+    const computerChoice = choices[randomIndex];
+
+    const result = getResult(choice, computerChoice);
+
+    document.getElementById('result').innerHTML = `You chose ${choice}. Computer chose ${computerChoice}. Result: ${result}`;
+}
+
+function getResult(user, computer) {
+    if (user === computer) {
+        return 'It\'s a tie!';
+    }
+
+    if ((user === 'rock' && computer === 'scissors') ||
+        (user === 'paper' && computer === 'rock') ||
+        (user === 'scissors' && computer === 'paper')) {
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
+}
